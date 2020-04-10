a1ed56b44618b6aee209f14773132592d14f76c0
walexbarnes <walexbarnes@gmail.com>
2020-04-10 16:03:59 -0500

    chanign css for legend

--- MODULE_13_CHALLENGE/static/js/logicStep3.js
+++ MODULE_13_CHALLENGE/static/js/logicStep3.js
@@ -573,33 +573,36 @@ L.geoJson(data, {
   }
 }).addTo(map);
 earthquakes.addTo(map)
-// Create a legend control object.
+// Here we create a legend control object.
+// Here we create a legend control object.
 let legend = L.control({
-	position: "bottomright"
-  });
+  position: "bottomright"
+});
 
-
-  // Then add all the details for the legend.
+// Then add all the details for the legend
 legend.onAdd = function() {
-	let div = L.DomUtil.create("div", "info legend");
-	const magnitudes = [0, 1, 2, 3, 4, 5];
-const colors = [
-  "#98ee00",
-  "#d4ee00",
-  "#eecc00",
-  "#ee9c00",
-  "#ea822c",
-  "#ea2c2c"
-];
+  let div = L.DomUtil.create("div", "info legend");
+
+  const magnitudes = [0, 1, 2, 3, 4, 5];
+  const colors = [
+    "#98ee00",
+    "#d4ee00",
+    "#eecc00",
+    "#ee9c00",
+    "#ea822c",
+    "#ea2c2c"
+  ];
 // Looping through our intervals to generate a label with a colored square for each interval.
 for (var i = 0; i < magnitudes.length; i++) {
-	div.innerHTML +=
-	  " " +
-	  magnitudes[i] + (magnitudes[i + 1] ? "–" + magnitudes[i + 1] + "" : "+");
- }
-  return div;
+  console.log(colors[i]);
+  div.innerHTML +=
+    "<i style='background: " + colors[i] + "'></i> " +
+    magnitudes[i] + (magnitudes[i + 1] ? "–" + magnitudes[i + 1] + "<br>" : "+");
+}
+return div;
 };
 
+// Finally, we our legend to the map.
 legend.addTo(map);
 		
 L.geoJson(lines, {
